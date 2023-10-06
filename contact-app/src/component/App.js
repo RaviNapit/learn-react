3ee3a782f9a30fd9fc1e3448fff2a4e0ab0a672d
Ravi Napit <ravi.napit@xecurify.com>
2023-10-06 12:37:10 +0530

    props

--- contact-app/src/component/App.js
+++ contact-app/src/component/App.js
@@ -2,12 +2,27 @@ import Header from './Header'
 import AddContact from './AddContact'
 import ContactList  from './ContactList' 
 
+
 function App() {
+  
+  const contactList = [
+    {
+      id:"1",
+      name:"Ravi Napit",
+      email:"ravi.napit@xecurify.com"
+    },
+    {
+      id:"2",
+      name:"Rajesh Biswas ",
+      email:"rajesh.biswas@xecurify.com"
+    }
+  ]
+
   return (
     <div>
       <Header />
       <AddContact />
-      <ContactList />
+      <ContactList contacts={contactList}/>
     </div>
   );
 }
