007905eac971cac71d8791fca850424282d5875f
Ravi Napit <ravi.napit@xecurify.com>
2023-10-06 13:13:38 +0530

    change repeted code to component

---
+++ contact-app/src/component/ContactCard.js
@@ -0,0 +1,13 @@
+const ContactCard = (props) => {
+    return(
+        <div className = "item">
+            <div className="content">
+                <div className="header"> {props.contact.name} </div>
+                <div> {props.contact.email} </div>
+            </div>
+            <i className="trash alternate outline icon"></i>
+        </div>
+    )
+}
+
+export default ContactCard
