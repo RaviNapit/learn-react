45a46fec95061910aed0215081079916790328f7
Ravi Napit <ravi.napit@xecurify.com>
2023-10-07 23:53:03 +0530

    useEffect Hook

--- contact-app/src/component/App.js
+++ contact-app/src/component/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './Header'
 import AddContact from './AddContact'
 import ContactList  from './ContactList' 
@@ -7,6 +7,19 @@ import ContactList  from './ContactList'
 function App() {
   
   const [contacts, setContacts] = useState([]) 
+  const LOCAL_STORAGE_KEY = 'contacts'
+  
+  useEffect(()=>{
+    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    console.log('useEffect', retrieveContacts);
+    if(retrieveContacts.length !=0 ) {
+      setContacts(retrieveContacts);
+    }
+  },[])
+
+  useEffect(()=>{
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
+  }, [contacts]);
 
   const contactHandler = (contact) => {
     console.log(contact)
--- contact-app/src/component/ContactList.js
+++ contact-app/src/component/ContactList.js
@@ -1,6 +1,6 @@
 import ContactCard from "./ContactCard"
 const ContactList = (props) => {
-
+    console.log("ContactCard", props.contacts)
     const renderContactList = props.contacts.map((contact) => {
         return(
             < ContactCard contact={contact}/>
